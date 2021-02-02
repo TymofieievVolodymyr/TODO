bc4601b78e9246e8639bbf9f3c1430148d977ab4
Tymofieiev <vovanium.timofeev@gmail.com>
2021-02-02 16:40:41 +0200

    start point

---
+++ app/index.js
@@ -0,0 +1,10 @@
+const app = (root) => {
+
+    const render = () => {
+        root.innerHTML = `Hello TODO`;
+    };
+
+    render();
+};
+
+export default app;
---
+++ index.js
@@ -0,0 +1,9 @@
+import app from './app';
+
+const root = document.getElementById('root');
+
+if (!root) {
+    throw new Error('The root element is undefined');
+}
+
+app(root);
