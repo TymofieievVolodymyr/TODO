7b286bd02783f8e349433c83a023212b921374a7
Tymofieiev <vovanium.timofeev@gmail.com>
2021-02-05 13:14:11 +0200

    Del commented code

--- app/view/listItemView/index.js
+++ app/view/listItemView/index.js
@@ -10,16 +10,3 @@ export default class ListItemView {
     return this;
   }
 }
-
-// export default class ListItemView {
-//   constructor(toDoItem) {
-//     this.liElement = document.createElement('li');
-//     this.span = document.createElement('span');
-//     this.liElement.append(this.span);
-//     this.spanItemContent = this.liElement.querySelector('span');
-//     this.liElement.id = toDoItem.id;
-//     this.spanItemContent.textContent = toDoItem.text;
-//   }
-//
-// }
-
