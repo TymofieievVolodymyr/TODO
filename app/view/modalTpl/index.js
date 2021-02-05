ea836e07f651b86a7cc69c8dbdd49147a855fe5d
Tymofieiev <vovanium.timofeev@gmail.com>
2021-02-05 23:07:58 +0200

    add styles

--- app/view/modalTpl/index.js
+++ app/view/modalTpl/index.js
@@ -1,13 +1,6 @@
-// export const modalTpl = `
-//   <div class="modal">
-//     <h2>Hi I am a modal!</h2>
-//   </div>
-// `;
-
-
 export const modalTpl = `
-<div class="modal">
-  <p class="message">Look at this fancy pop-up</p>
+<div class="modal">  
+  <input type="text">
   <div class="options">
     <button class="btn">Yes</button>
     <button class="btn">No</button>
