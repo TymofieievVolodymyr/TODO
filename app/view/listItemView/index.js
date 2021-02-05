ebb08997f2ed7fa987281f2f02c71976b83311af
Tymofieiev <vovanium.timofeev@gmail.com>
2021-02-05 13:51:52 +0200

    Refactored ListItemView with DOM class methods

--- app/view/listItemView/index.js
+++ app/view/listItemView/index.js
@@ -1,9 +1,11 @@
+import DOM from "../../utilities/DOM";
+
 export default class ListItemView {
   render(toDoItem) {
-    this.liElement = document.createElement('li');
-    this.span = document.createElement('span');
-    this.liElement.append(this.span);
-    this.spanItemContent = this.liElement.querySelector('span');
+    this.liElement = DOM.createElement('li');
+    this.span = DOM.createElement('span');
+    DOM.append(this.liElement, this.span);
+    this.spanItemContent = DOM.getElement(this.liElement, 'span');
     this.liElement.id = toDoItem.id;
     this.spanItemContent.textContent = toDoItem.text;
 
