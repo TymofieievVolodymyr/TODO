916e498a477c8ad226a19465d7391c17213379b8
Tymofieiev <vovanium.timofeev@gmail.com>
2021-02-08 11:48:50 +0200

    Added styles and refactored template

--- app/view/formTpl/index.js
+++ app/view/formTpl/index.js
@@ -8,6 +8,14 @@ export const formTpl = `
   </form>
   <ul class="todo-list">
   </ul>
+  <footer class="footer">
+  <ul class="filter">
+    <li><a class="all">All</a></li>
+    <li><a class="active">Active</a></li>
+    <li><a class="completed">Completed</a></li>
+    <li><a class="clear-completed">Clear Completed</a></li>
+  </ul>
+  </footer>
 `;
 
 
