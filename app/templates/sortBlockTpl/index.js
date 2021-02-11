18dd0d0e01ddec4b95898b228027b81cf23e05e7
Tymofieiev <vovanium.timofeev@gmail.com>
2021-02-11 15:57:45 +0200

    added filter logic according to US008

--- app/templates/sortBlockTpl/index.js
+++ app/templates/sortBlockTpl/index.js
@@ -14,16 +14,16 @@ export const sortBlockTpl = `
     <form>
       <ul class="flex-outer">
         <li>
-          <label for="first-name">Filter By Text</label>
-          <input type="text" id="first-name" placeholder="Enter your text">
+          <label for="filterText">Filter By Text</label>
+          <input type="text" id="filterText" placeholder="Enter your text">
         </li>
         <li>
-          <label for="last-name">Filter By Starting Date</label>
-          <input type="text" id="last-name" placeholder="Enter Start date">
+          <label for="startDate">Filter By Starting Date</label>
+          <input type="date" id="startDate" placeholder="Enter Start date">
         </li>
         <li>
-          <label for="email">Filter By Expiration Date</label>
-          <input type="email" id="email" placeholder="Enter End date">
+          <label for="expirationDate">Filter By Expiration Date</label>
+          <input type="date" id="expirationDate" placeholder="Enter End date">
         </li>
       </ul>
     </form>
